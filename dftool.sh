062898d1ecec0df476883eb7adc3a8d9086ea9cf
Devan Muthappa <110847602+Dxvan06@users.noreply.github.com>
2024-07-11 20:43:11 +0530

    Update dftool.sh

--- dftool.sh
+++ dftool.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Create dftools directory if it doesn't exist
+install_dir="$HOME/dftools"
+mkdir -p $install_dir
+
 # Array of tools to check and install with their package names or install methods
 declare -A tools=(
     ["sleuthkit"]="sleuthkit"
@@ -16,34 +20,46 @@ declare -A tools=(
 
 # Function to check if a tool is installed
 check_tool() {
-    if command -v $1 &> /dev/null
+    local tool_name=$1
+    local tool_package=${tools[$1]}
+    local tool_install_path="$install_dir/$tool_name"
+
+    if command -v $tool_name &> /dev/null
     then
-        echo "$1 is already installed."
+        echo "$tool_name is already installed."
     else
-        echo "$1 is not installed. Attempting to install ${tools[$1]}..."
-        sudo apt-get update && sudo apt-get install -y ${tools[$1]}
+        echo "$tool_name is not installed. Attempting to install $tool_package..."
+        sudo apt-get update && sudo apt-get install -y $tool_package
+        if [ $? -eq 0 ]; then
+            echo "$tool_name installed successfully."
+        else
+            echo "Failed to install $tool_name."
+            return 1
+        fi
     fi
 }
 
 # Check and install tools available via apt-get
 for tool in "${!tools[@]}"
 do
-    check_tool $tool
+    check_tool $tool || exit 1
 done
 
 # Special handling for tools not available via apt-get
 
-# Install Volatility
+# Install Volatility 3
 install_volatility() {
-    if [ -d "/usr/local/volatility3" ]; then
+    local tool_name="volatility"
+    local tool_package="volatility3"
+    local tool_install_path="$install_dir/$tool_name"
+
+    if [ -d "$tool_install_path" ]; then
         echo "Volatility 3 is already installed."
     else
         echo "Volatility 3 is not installed. Attempting to install..."
-        sudo apt-get update
         sudo apt-get install -y git python3 python3-pip
-        git clone https://github.com/volatilityfoundation/volatility3.git
-        sudo mv volatility3 /usr/local/volatility3
-        sudo pip3 install -r /usr/local/volatility3/requirements.txt
+        git clone https://github.com/volatilityfoundation/volatility3.git $tool_install_path
+        sudo pip3 install -r $tool_install_path/requirements.txt
         echo "Volatility 3 installed successfully."
     fi
 }
